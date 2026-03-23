32a65032036debb426955fde40952b1cf779b92e
Abdullateef.Suleiman <abdullateef.suleiman@interswitchng.com>
2026-03-23 22:28:38 +0100

    implemented a basic chat agent

--- go-ai.go
+++ go-ai.go
@@ -1,28 +1,54 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
+	dotenv "github.com/joho/godotenv" //import a module with alias
 	"github.com/openai/openai-go"
-	dotenv "github.com/joho/godotenv" // import a module with alias
-
 )
 
 func main() {
 
 	dotenv.Load()
 	client := openai.NewClient()
-	resp, err := client.Chat.Completions.New(context.Background(), openai.ChatCompletionNewParams{
-		Model: "gpt-4o-mini",
-		Messages: []openai.ChatCompletionMessageParamUnion{
-			openai.UserMessage("How many coffees should a man drink per day?"),
-		},
-	})
+	ctx := context.Background()
 
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
+	// Read input from command prompt
+	for {
+		fmt.Print("\nEnter question > ")
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(query(ctx, client, line))
 	}
+}
 
-	fmt.Println(resp.Choices[0].Message.Content)
+func makeRequest(questions string) openai.ChatCompletionNewParams {
+	maxToken, _ := strconv.Atoi(os.Getenv("MAX_TOKEN"))
+	temperature, _ := strconv.ParseFloat(os.Getenv("TEMPERATURE"), 32)
+	model := os.Getenv("GPT_MODEL")
+
+	return openai.ChatCompletionNewParams{
+		Model:       model,
+		MaxTokens:   openai.Int(int64(maxToken)),
+		Temperature: openai.Float(temperature),
+		Messages: []openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(questions),
+		},
+	}
+}
+
+func query(ctx context.Context, client openai.Client, question string) string {
+	resp, err := client.Chat.Completions.New(ctx, makeRequest(question))
+	if err != nil {
+		log.Fatal("Error => ", err)
+	}
+	return resp.Choices[0].Message.Content
 }
